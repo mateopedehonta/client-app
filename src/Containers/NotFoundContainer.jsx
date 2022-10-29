3094b774e88fff974788828b33c42bd8e11d638c
mateopedehonta <mateopedehonta@gmail.com>
2022-10-29 18:47:47 -0300

    first commit

--- src/Containers/NotFoundContainer.jsx
+++ src/Containers/NotFoundContainer.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const NotFoundContainer = () => {
-    return ( <>
-    not found 
-    <Link to={'/mateo'}>go mateo</Link>
-    </> );
-}
- 
+  const [link, setLink] = useState({
+    link: "",
+  });
+  const addData = (e) => {
+    setLink({
+      ...link,
+      [e.target.name]: e.target.value,
+    });
+  };
+  return (
+    <>
+      <input type="text" name="link" onChange={addData} />
+      <Link to={`/${link.link}`}>go mateo</Link>
+    </>
+  );
+};
+
 export default NotFoundContainer;
